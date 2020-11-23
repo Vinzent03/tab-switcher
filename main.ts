30b642aebb3cb7b50c9709d5980d3032e8eec3de
Phillip <bronzel.phillip@gmail.com>
2020-11-23 10:48:29 +0100

    0.0.2

--- main.ts
+++ main.ts
@@ -29,6 +29,31 @@ export default class CycleThroughPanes extends Plugin {
 
         });
 
+        this.addCommand({
+            id: 'cycle-through-panes-reverse',
+            name: 'Cycle through panes (Reverse)',
+            callback: () => {
+                let active = this.app.workspace.activeLeaf;
+                let leafs: WorkspaceLeaf[] = []
+                this.app.workspace.iterateAllLeaves((leaf) => {
+                    if (leaf.getViewState().type == "markdown")
+                        leafs.push(leaf);
+                })
+                let index = leafs.indexOf(active);
+				if (index == 0)
+                    this.app.workspace.setActiveLeaf(leafs[leafs.length-1])
+                else
+                    this.app.workspace.setActiveLeaf(leafs[index - 1])
+
+            }, hotkeys: [
+                {
+                    modifiers: ["Mod", "Shift"],
+                    key: "Tab"
+                }
+            ]
+
+        });
+
 	}
 
 	onunload() {
