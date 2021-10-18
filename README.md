96cb16a42111a488ae71f827fa215b6a838dbea4
phibr0 <bronzel.phillip@gmail.com>
2021-10-18 22:07:36 +0200

    Simplify code, release new version

---
+++ README.md
@@ -0,0 +1,25 @@
+# Cycle through Panes ![GitHub tag (latest by date)](https://img.shields.io/github/v/tag/phibr0/cycle-through-panes) ![](https://tokei.rs/b1/github/phibr0/cycle-through-panes) ![GitHub all releases](https://img.shields.io/github/downloads/phibr0/cycle-through-panes/total)
+
+__This Plugin uses Code from [Vinadon](https://github.com/Vinzent03)__
+
+_In Combination with Mr. Jackphils Jump to Link plugin and Vim Keybindings, this plugin allows you to control Obsidian entirely without a Mouse!_
+
+## How to use
+
+- `Ctrl + Tab` to switch open Editor/Preview Tabs
+- `Ctrl + Shift + Tab` to switch open Editor/Preview Tabs in reverse order
+- Set hotkey in settings to focus on last active pane
+
+### Important
+
+The only supported pane type is markdown, because I thought its the only pane type where it makes sense to switch to.
+
+
+---
+### Manually installing the plugin
+
+- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/cycle-through-panes/`.
+
+### API Documentation
+
+See https://github.com/obsidianmd/obsidian-api
---
+++ main.ts
@@ -0,0 +1,94 @@
+import { Plugin } from 'obsidian';
+
+export default class CycleThroughPanes extends Plugin {
+	lastPanes: string[] = [];
+
+	onload() {
+		console.log('loading plugin: Cycle through panes');
+
+		this.addCommand({
+			id: 'cycle-through-panes',
+			name: 'Cycle through Panes',
+			checkCallback: (checking: boolean) => {
+				let active = this.app.workspace.activeLeaf;
+				if (active) {
+					if (!checking) {
+						let leafs = this.app.workspace.getLeavesOfType("markdown");
+						let index = leafs.indexOf(active);
+						if (index === leafs.length - 1) {
+							this.app.workspace.setActiveLeaf(leafs[0], true, true);
+						} else {
+							this.app.workspace.setActiveLeaf(leafs[index + 1], true, true);
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+		});
+
+		this.addCommand({
+			id: 'cycle-through-panes-reverse',
+			name: 'Cycle through panes (Reverse)',
+			checkCallback: (checking: boolean) => {
+				let active = this.app.workspace.activeLeaf;
+				if (active) {
+					if (!checking) {
+						let leafs = this.app.workspace.getLeavesOfType("markdown");
+						let index = leafs.indexOf(active);
+						if (index) {
+							if (index === 0) {
+								this.app.workspace.setActiveLeaf(leafs[leafs.length - 1], true, true);
+							} else {
+								this.app.workspace.setActiveLeaf(leafs[index - 1], true, true);
+							}
+						}
+					}
+					return true;
+				}
+				return false;
+			}
+		});
+
+		//fires when a new file is opened or the focus switches to another pane
+		this.app.workspace.on("file-open", () => {
+			//use just markdown panes
+			if (this.app.workspace.activeLeaf.getViewState().type != "markdown") {
+				return;
+			}
+			//if a file gets opened in current pane
+			if (this.lastPanes?.last() == (this.app.workspace.activeLeaf as any).id) {
+				return;
+			}
+			//keep a history of 10 panes
+			if (this.lastPanes.length > 10) {
+				this.lastPanes.splice(0, 1);
+			}
+			//add current pane to history
+			this.lastPanes.push((this.app.workspace.activeLeaf as any).id);
+		});
+
+		this.addCommand({
+			id: 'focus-on-last-active-pane',
+			name: 'Focus on last active pane',
+			callback: () => {
+				let leaf;
+				//Cycle thorough the history until a pane is still there and not the current pane
+				for (var i = 2; i <= this.lastPanes.length; i++) {
+					if (this.lastPanes[this.lastPanes.length - i] == this.lastPanes.last())
+						continue;
+					leaf = this.app.workspace.getLeafById(this.lastPanes[this.lastPanes.length - i]);
+					if (leaf) break;
+				}
+				if (leaf) {
+					this.app.workspace.setActiveLeaf(leaf, true, true);
+				}
+			}
+		});
+
+	}
+
+	onunload() {
+		console.log('unloading plugin: Cycle through panes');
+	}
+}
