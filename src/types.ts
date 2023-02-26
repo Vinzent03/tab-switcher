1ad29f1975c35a8db73cb4dd117b20101015d0bf
Vinzent <vinzent03@proton.me>
2023-02-26 13:43:52 +0100

    feat: add canvas as default view type

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,7 @@
 export interface Settings {
     viewTypes: string[];
-    onlyRootLeaves: boolean;
 }
 
 export const DEFAULT_SETTINGS: Settings = {
-    viewTypes: ['markdown'],
-    onlyRootLeaves: true,
-}
+    viewTypes: ['markdown', 'canvas'],
+};
